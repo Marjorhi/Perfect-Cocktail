34b897c133d7bb40cf96f4b9f5824fe92dcb911c
Marjorhi <marjoriejetajobe0524@gmail.com>
2021-12-13 16:19:45 +0800

    Create the Event Listeners to app.js

--- Cocktail Recipe/js/app.js
+++ Cocktail Recipe/js/app.js
@@ -2,3 +2,22 @@
 const ui = new UI(),
       cocktail = new CocktailAPI();
 
+// Create the Event Listeners
+function eventListeners(){
+    // Document Ready
+    document.addEventListener('DOMContentLoaded', documentReady);
+
+    // add event listeners when form is submitted
+    const searchForm = document.querySelector('#search-form');
+    if(searchForm) {
+        searchForm.addEventListener('submit', getCocktails);
+    }
+
+    // The results div listeners
+    const resultsDiv = document.querySelector('#results');
+    if(resultsDiv) {
+        resultsDiv.addEventListener('click', resultsDelegation)
+    }
+}
+
+eventListeners();
