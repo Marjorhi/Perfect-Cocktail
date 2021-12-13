682960bc6f97c5df017529f4a6c6eb70963050ce
Marjorhi <marjoriejetajobe0524@gmail.com>
2021-12-13 16:06:04 +0800

    Get recipe by name to CocktailAPI.js

---
+++ Cocktail Recipe/js/CocktailAPI.js
@@ -0,0 +1,16 @@
+const proxyURL = "https://cors-anywhere.herokuapp.com/";
+
+class CocktailAPI{
+    // Get recipe by name
+    async getDrinksByName(name){
+        // Search by name
+        const apiResponse = await fetch(`${proxyURL}https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+        
+        // Returns a json response
+        const cocktails = await apiResponse.json();
+        
+        return {
+            cocktails
+        }
+        
+    }
