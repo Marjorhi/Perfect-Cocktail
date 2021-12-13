efbd0a2324ccb80ca68289c0a9b7058bafa28333
Marjorhi <marjoriejetajobe0524@gmail.com>
2021-12-13 16:36:06 +0800

    Get cocktails function to app.js

--- Cocktail Recipe/js/app.js
+++ Cocktail Recipe/js/app.js
@@ -21,3 +21,58 @@ function eventListeners(){
 }
 
 eventListeners();
+
+
+// Get cocktails function
+function getCocktails(e){
+    e.preventDefault();
+
+    const searchTerm = document.querySelector('#search').value;
+
+    // Check something is on the search input
+    if(searchTerm === ''){
+        // Call user interface print message
+        ui.printMessage('Please add something into the form', 'danger');
+    }else {
+        // Server response from promise
+        let serverResponse;
+
+        // Type of search (ingredients, cocktails, or name)
+        const type = document.querySelector('#type').value;
+
+        // Evaluate the type of method and then execute the query
+
+        switch(type) {
+            case 'name':
+                serverResponse = cocktail.getDrinksByName(searchTerm);
+                break;
+            case 'ingredient':
+                serverResponse = cocktail.getDrinksByIngredient(searchTerm);
+                break;
+            case 'category':
+                serverResponse = cocktail.getDrinksByCategory(searchTerm);
+                break;
+            case 'alcohol':
+                serverResponse = cocktail.getDrinksByAlcohol(searchTerm);
+                break;
+        }
+
+        ui.clearResults();
+
+        // Query by the name of the drink
+        serverResponse.then(cocktails => {
+            if(cocktails.cocktails.drinks === null) {
+                // Nothing exists
+                ui.printMessage('There\'re no result, try a different term', 'danger');
+            }else {
+                if(type === 'name') {
+                    // Display with ingredients
+                    ui.displayDrinksWithIngredients(cocktails.cocktails.drinks);
+                }else {
+                    // Display without ingredients (category, alcohol, ingredient)
+                    ui.displayDrinks(cocktails.cocktails.drinks);
+                }
+            }
+        })
+    }
+}
